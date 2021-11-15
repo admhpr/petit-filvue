51383b2a47877ef7fb9abcd5b0c008f897fa884e
adam <adam@harpur.io>
2021-11-15 09:20:21 +0000

    chore: implement mount

--- src/core/runtime.ts
+++ src/core/runtime.ts
@@ -5,13 +5,13 @@ type Props = {
 type VNode = {
     tag: keyof HTMLElementTagNameMap,
     props: Props | null,
-    children: unknown[]
+    children: VNode[] | string
 }
 
 function h(
   tag: keyof HTMLElementTagNameMap,
   props: Props | null,
-  children: unknown[]
+  children: VNode[] | string
 ): VNode {
   return {
     tag,
@@ -20,8 +20,26 @@ function h(
   }
 }
 
-const vdom = h('div', {class: 'red'}, [h('span', null, ['hello'])])
+const vdom = h('div', {class: 'red'}, [h('span', null, 'hello')])
 
-function mount(vnode: VNode, container){
+function mount(vnode: VNode, container: HTMLElement){
     const el = document.createElement(vnode.tag)
+    // has props
+    if(vnode.props){
+        for(const key in vnode.props){
+            const value = vnode.props[key]
+            el.setAttribute(key, value)
+        }
+    }
+    // has children
+    if(vnode.children){
+        if(typeof vnode.children === "string"){
+            el.textContent = vnode.children
+        }else {
+            vnode.children.forEach(child => {
+                mount(child, el)
+            })
+        }
+    }
+    container.appendChild(el)
 }
